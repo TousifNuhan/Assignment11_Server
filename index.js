071a8b0879ddde07fe90682c2229cc93aff88e1e
Gazi Mubtasim Tousif Nuhan <mubtasimtousif@gmail.com>
2025-06-07 17:00:42 +0600

    Completed mySubmittedAssignments api and allSubmittedAssignments api

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port= process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l6latif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,7 +26,56 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+
+
+    const OnlineGroupStudyDB = client.db('OnlineGpDB').collection('GroupStudy')
+    const OnlineGroupStudyDBs = client.db('OnlineGpDB').collection('SubmittedAssignments')
+
+    app.get('/createAssignments/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await OnlineGroupStudyDB.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/createAssignments', async (req, res) => {
+      const cursor = OnlineGroupStudyDB.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/createAssignments', async (req, res) => {
+      const newAssignments = req.body
+      console.log(newAssignments)
+      const result = await OnlineGroupStudyDB.insertOne(newAssignments)
+      res.send(result)
+    })
+
+    // submittedAssignments
+    app.post('/submittedAssignments', async (req, res) => {
+      const data = req.body
+      console.log(data)
+      const result = await OnlineGroupStudyDBs.insertOne(data)
+      res.send(result)
+    })
+
+    // specific user's submitted assignments
+    app.get('/mySubmittedAssignments', async (req, res) => {
+      const email = req.query.email
+      // console.log(email)
+      const query = { Email: email }
+      const result = await OnlineGroupStudyDBs.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/allSubmittedAssignments', async (req, res) => {
+      const query = { Status: 'Pending' }
+      const result= await OnlineGroupStudyDBs.find(query).toArray()
+      res.send(result)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,11 +85,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('running')
+app.get('/', (req, res) => {
+  res.send('running')
 })
 
-app.listen(port, ()=>{
-    console.log(`server ${port}`)
+app.listen(port, () => {
+  console.log(`server ${port}`)
 })
 
